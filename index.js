22504359a65e01e76f26c67f28a72fe2bd97b012
Ben <benjaminadk@gmail.com>
2017-10-22 02:45:00 +0000

    update

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/user");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, { useMongoClient: true }, () =>
+  console.log("server connected to mLab")
+);
+mongoose.set("debug", true);
+mongoose.Promise = global.Promise;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "dude" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
